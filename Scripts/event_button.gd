0ff20b6d78191e930fdb8c1aa91fda4dacafa9fe
iwdie <naveenworkstation@gmail.com>
2026-05-09 13:14:30 +0530

    working_out_the_level_end_mechanism

--- Scripts/event_button.gd
+++ Scripts/event_button.gd
@@ -14,5 +14,6 @@ func _process(delta: float) -> void:
 func _on_button_pressed() -> void:
 	print("dest_size:",ConveyerController.destination.size())
 	$"../broker".duplicate_events()
-	$"../Timer".start()
+	if  ConveyerController.olderlevels == false:
+		$"../Timer".start()
 	Level.initialise()
