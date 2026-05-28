18968d615dbf42e4e70140d7f59a9cfd46c6bf91
iwdie <naveenworkstation@gmail.com>
2026-05-28 18:41:28 +0530

    Stopping the movement when game is in the transition state.

--- Scripts/event_button.gd
+++ Scripts/event_button.gd
@@ -6,6 +6,7 @@ var Levelisoptimised:bool
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	process_mode = Node.PROCESS_MODE_ALWAYS
 	Levelisoptimised = false # Replace with function body.
 
 
@@ -15,6 +16,7 @@ func _process(delta: float) -> void:
 
 
 func _on_button_pressed() -> void:
+	
 	print("dest_size:",ConveyerController.destination.size())
 	$"../broker".duplicate_events()
 	print(ConveyerController.conveyerInd)
